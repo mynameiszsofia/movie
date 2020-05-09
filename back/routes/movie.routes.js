4b4701744d7ced0e59f67d1f6f0e80854d314be7
Cihangir Bozdogan <cihangirrbozzd@gmail.com>
2020-05-09 23:31:46 +0100

    first and last

--- back/routes/movie.routes.js
+++ back/routes/movie.routes.js
@@ -2,14 +2,8 @@ const express = require("express");
 const router = express.Router();
 const movieController = require("../controllers/movie.controllers");
 
-router.post("/", movieController.createMovie);
+router.post("/movies", movieController.createMovie);
 router.get("/", movieController.getMovies);
-
-
-
-
-
-
 router.get("/:id", movieController.getMovieById);
 
 module.exports = router;
--- back/server.js
+++ back/server.js
@@ -1,30 +1,47 @@
 const express = require("express");
 const mongoose = require("mongoose");
-const app = express();/* 
-const movieRoutes = require("./routes/movie.routes"); */
+const bodyParser = require("body-parser");
 
-const connect = () => {
-    return mongoose.connect("mongodb://localhost:27017/movie", {
-        useNewUrlParser: true,
-        useUnifiedTopology: true
-    });
-};
+const app = express();
+const movieRoutes = require("./routes/movie.routes");
+const movieService = require("./models/movie.models");
 
-connect();
+mongoose.set("useNewUrlParser", true);
+mongoose.set("useFindAndModify", false);
+mongoose.set("useCreateIndex", true);
+mongoose.set("useUnifiedTopology", true);
+mongoose
+  .connect("mongodb://localhost/movie")
+  .then(console.log("Database Connection Success"))
+  .catch((e) => console.log(e));
 
-app.use(express.json());/* 
-app.use("/movie", movieRoutes); */
+app.use(bodyParser.json({ limit: "50mb" }));
+app.use(
+  bodyParser.urlencoded({
+    limit: "50mb",
+    extended: true,
+    parameterLimit: 50000,
+  })
+);
 
-app.post('/api', (request, response) => {
-    const hei = request.body;
-    console.log(request.body);
-    response.json({
-        status: 'success',
-        yes: hei.data
-    });
+app.post("/movies", async (req, res) => {
+  const movie = JSON.parse(req.body.movie);
+  const data = {
+    name: movie.name,
+    type: movie.type,
+    genres: movie.genres[0],
+    rating: JSON.stringify(movie.rating), // this data is weird! log it to consoole
+    image: movie.image.original,
+  };
+  const newMovie = new movieService(data);
+  newMovie
+    .save()
+    .then(() => {
+      res.json({ result: movie });
+    })
+    .catch((e) => console.log(e));
 });
 
+app.use("/movie", movieRoutes);
 
-app.listen(5000, () => {
-    console.log("app is running on 5000");
-})
+app.listen(5000, () => console.log("app is running on 5000"));
